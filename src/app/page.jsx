877a6501ffa93d94775110b1e61955d998f6c729
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-07-27 20:16:01 +0530

    Initial commit - Next.js setup

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1>ho next js</h1>
+      <h1 className="text-3xl font-bold underline bg-red-400 ">Hello world!</h1>
+      <h1 className="text-"></h1>
+
+      <h1></h1>
+    </>
+  );
+}
