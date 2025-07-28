a8977b47f10ef5f8301c1c80cf64afb762ad7a80
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-07-28 21:02:27 +0530

    learned how to import  colrs withput tailwind config js  . ite i use global.css 🔴✅🟢

--- src/app/(adminhome)/admin/page.jsx
+++ src/app/(adminhome)/admin/page.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import "../../globals.css";
 
 export default function AdminDashborad() {
-  return <div> Admin Dashborad page</div>;
+  return (
+    <div className="bg-[var(--color-primary)] text-[var(--color-primary)] p-6 rounded-m">
+      {" "}
+      Admin Dashborad page
+    </div>
+  );
 }
